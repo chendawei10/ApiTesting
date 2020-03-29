7851299f55fc58ada5ee6666ac4d1971aa0a7fea
chendawei10 <36591217@qq.com>
2020-03-29 21:02:00 +0800

    init

---
+++ testcase/test_requests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import requests
+import logging
+import pytest
+import json
+
+class TestRequests(object):
+    logging.basicConfig(level=logging.INFO)
+    def test_get(self):
+        r = requests.get("https://testerhome.com/api/v3/topics.json?limit=2")
+        logging.info(r)
+        logging.info(r.text)
+        logging.info(json.dumps(r.json(),indent=2))
